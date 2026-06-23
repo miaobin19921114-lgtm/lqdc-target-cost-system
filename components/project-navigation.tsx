007e1b4e4e20b14e59fe94eb214e97657b7d3886
miaobin19921114-lgtm <miaobin19921114@gmail.com>
2026-06-23 20:48:11 +0800

    add project rule snapshot navigation

--- components/project-navigation.tsx
+++ components/project-navigation.tsx
@@ -28,6 +28,7 @@ export const projectNavGroups = [
       ['围墙出入口', 'wall-gate-details', 'done'],
       ['规则模板中心', 'rule-template-center', 'done'],
       ['模板科目开关', 'template-subject-switches', 'done'],
+      ['项目规则快照', 'project-rule-snapshot', 'done'],
       ['模板字段需求', 'template-field-requirements', 'done'],
       ['字段定义库', 'template-field-definitions', 'done'],
       ['成本科目及测算规则库', 'cost-calculation-rules', 'done'],
