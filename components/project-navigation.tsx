2455e2328954844ccd9a7576034ee8e63b97545c
miaobin19921114-lgtm <miaobin19921114@gmail.com>
2026-06-23 20:02:40 +0800

    add field definition library navigation

--- components/project-navigation.tsx
+++ components/project-navigation.tsx
@@ -28,6 +28,7 @@ export const projectNavGroups = [
       ['围墙出入口', 'wall-gate-details', 'done'],
       ['规则模板中心', 'rule-template-center', 'done'],
       ['模板字段需求', 'template-field-requirements', 'done'],
+      ['字段定义库', 'template-field-definitions', 'done'],
       ['成本科目及测算规则库', 'cost-calculation-rules', 'done'],
       ['量价指标库', 'price-library', 'done'],
       ['Excel科目映射', 'cost-mapping', 'done']
